95a0c9e725ffea6c7a27ed7e1634676638083737
Wesley Martin <wes@plusnarrative.com>
2020-09-26 21:42:06 +0200

    Adding ability to list keys and choose the key by it's index

--- main.go
+++ main.go
@@ -17,12 +17,27 @@ import (
 	badger "github.com/dgraph-io/badger/v2"
 )
 
+var db *badger.DB
+
 func main() {
+	var err error
 	flag.Parse()
 	args := flag.Args()
 
+	db, err = initDb()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
 	if len(args) == 0 {
-		check(errors.New("Please add something, anything... I don't know what you want from me."))
+		fmt.Println(errors.New("Please add something, anything... I don't know what you want from me."))
+		return
+	}
+
+	if index, err := strconv.Atoi(args[0]); err == nil {
+		getOtpByIndex(index)
 		return
 	}
 
@@ -31,28 +46,48 @@ func main() {
 		return
 	}
 
+	if args[0] == "ls" {
+		listKeys()
+		return
+	}
+
 	getOtp(args[0])
 }
 
-func addToken(key string, secret string) {
-	db, err := badger.Open(badger.DefaultOptions("/tmp/gotp"))
-	if err != nil {
-		check(err)
-	}
-	defer db.Close()
+func listKeys() {
+	err := db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		i := 1
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			fmt.Printf("%d: %s\n", i, k)
+			i++
+		}
+		return nil
+	})
 
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func addToken(key string, secret string) {
 	txn := db.NewTransaction(true)
 	defer txn.Discard()
 
 	// Use the transaction...
-	err = txn.Set([]byte(key), []byte(secret))
+	err := txn.Set([]byte(key), []byte(secret))
 	if err != nil {
-		check(err)
+		fmt.Println(err)
 	}
 
 	// Commit the transaction and check for error.
 	if err := txn.Commit(); err != nil {
-		check(err)
+		fmt.Println(err)
 		return
 	}
 
@@ -60,31 +95,73 @@ func addToken(key string, secret string) {
 }
 
 func getOtp(key string) {
-	db, err := badger.Open(badger.DefaultOptions("/tmp/gotp"))
-	if err != nil {
-		check(err)
-	}
-	defer db.Close()
-
-	err = db.View(func(txn *badger.Txn) error {
-		token, err := txn.Get([]byte(key))
+	err := db.View(func(txn *badger.Txn) error {
+		var token string
+		item, err := txn.Get([]byte(key))
 		if err != nil {
 			return err
 		}
 
-		otp := getTOTPToken(token.String())
+		err = item.Value(func(val []byte) error {
+			token = string(val)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		otp := getTOTPToken(token)
 
 		fmt.Println("Your otp is:" + otp)
 		fmt.Println(otp + " has been copied to your clipboard")
 
 		//Copies the otp generated to your clipboard
 		err = exec.Command("bash", "-c", fmt.Sprintf("echo %s | tr -d \"\n, \" | pbcopy", otp)).Run()
-		check(err)
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
 
-	check(err)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getOtpByIndex(index int) {
+	var key string
+	err := db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		i := 1
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		for it.Rewind(); it.Valid(); it.Next() {
+			if index != i {
+				i++
+				continue
+			}
+
+			item := it.Item()
+			key = string(item.Key())
+
+			it.Close()
+		}
+
+		return nil
+	})
+
+	if key == "" {
+		fmt.Println("Could not find your key")
+		return
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	getOtp(key)
 }
 
 func getTOTPToken(secret string) string {
@@ -94,11 +171,12 @@ func getTOTPToken(secret string) string {
 }
 
 func getHOTPToken(secret string, interval int64) string {
-
 	//Converts secret to base32 Encoding. Base32 encoding desires a 32-character
 	//subset of the twenty-six letters A–Z and ten digits 0–9
 	key, err := base32.StdEncoding.DecodeString(strings.ToUpper(secret))
-	check(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	bs := make([]byte, 8)
 	binary.BigEndian.PutUint64(bs, uint64(interval))
 
@@ -118,7 +196,9 @@ func getHOTPToken(secret string, interval int64) string {
 	r := bytes.NewReader(h[o : o+4])
 	err = binary.Read(r, binary.BigEndian, &header)
 
-	check(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	//Ignore most significant bits as per RFC 4226.
 	//Takes division from one million to generate a remainder less than < 7 digits
 	h12 := (int(header) & 0x7fffffff) % 1000000
@@ -127,12 +207,6 @@ func getHOTPToken(secret string, interval int64) string {
 	otp := strconv.Itoa(int(h12))
 
 	return prefix0(otp)
-}
-
-func check(e error) {
-	if e != nil {
-		fmt.Println(e)
-	}
 }
 
 //Append extra 0s if the length of otp is less than 6
@@ -145,4 +219,13 @@ func prefix0(otp string) string {
 		otp = "0" + otp
 	}
 	return otp
+}
+
+func initDb() (*badger.DB, error) {
+	options := badger.DefaultOptions("/tmp/gotp")
+	options.Logger = nil
+
+	db, err := badger.Open(options)
+
+	return db, err
 }
